ae120f92adddc32c9a26d4ea59868d5d1859ffa9
Pablo Peña <pablo_penna7@hotmail.com>
2025-12-05 13:13:59 +0800

    feat: add camera and vibration cordova plugins

--- webapp/controller/Landing.controller.js
+++ webapp/controller/Landing.controller.js
@@ -6,6 +6,19 @@ sap.ui.define([
     return BaseController.extend("cordova-ui5-playground.controller.Landing", {
         onInit() {
             console.log("landing page onInit");
-        }
+        },
+
+        test1() {
+            console.log(cordova);
+            console.log(navigator);
+        },
+
+        test2() {
+            console.log("vibrating");
+            navigator.vibrate(3000);
+
+            console.log("taking a phto");
+            navigator.camera.getPicture(()=>console.log("SUCCESS!"), ()=>console.log("ERROR!"), {});
+        },
     });
 });
