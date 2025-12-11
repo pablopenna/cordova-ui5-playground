4d7cc8d43ca8f4a6c4d1983635844a24baa872e9
Pablo Peña <pablo_penna7@hotmail.com>
2025-12-11 11:40:32 +0800

    test: add debug logging

--- webapp/controller/KeyPressListener.js
+++ webapp/controller/KeyPressListener.js
@@ -12,25 +12,28 @@ sap.ui.define([
 
         static setup() {
             document.addEventListener('keypress', this.onKeyPress.bind(this));
-            // document.addEventListener('keydown', this.onKeyDown.bind(this));
+            document.addEventListener('keydown', this.onKeyDown.bind(this));
         }
 
         static onKeyPress(event) {
             const keyPressed = event.key;
-            // console.log("keypressed!", "event:", event, "key:", keyPressed);
+            console.log("keypressed!", "event:", event, "key:", keyPressed);
 
             // Only record keypresses if focused element is readOnly
             if(!document.activeElement.readOnly){ 
+                console.log("focused element is not read only");
                 return;
             }
 
             const focusedElement = event.target;
             if(this.elementBeingMonitored !== focusedElement) {
-                console.log("monitoring ", focusedElement);
+                console.log("changing focused element");
+                console.log("now monitoring: ", focusedElement);
                 this.monitorNewElement(focusedElement);
             }
 
             if(keyPressed === ENTER_KEY) {
+                console.log("Enter key pressed");
                 this.elementBeingMonitored.value = this.getTextFromInputBuffer();
                 this.clearInputBuffer();
             } else {
@@ -47,7 +50,7 @@ sap.ui.define([
 
         static monitorNewElement(element) {
             this.clearInputBuffer();
-            // this.abortController.abort(); // remove 'blur' event listener from previous monitored elements
+            this.abortController.abort(); // remove 'blur' event listener from previous monitored elements
 
             this.elementBeingMonitored = element;
             this.elementBeingMonitored.addEventListener(
@@ -62,14 +65,17 @@ sap.ui.define([
         }
 
         static clearInputBuffer() {
+            console.log("Cleaning input buffer");
             this.inputBuffer = [];
         }
 
         static addKeyToInputBuffer(key) {
+            console.log("Storing key in input buffer: ", keyPressed);
             this.inputBuffer.push(key);
         }
 
         static getTextFromInputBuffer() {
+            console.log("Getting text from buffer");
             return this.inputBuffer.join('');
         }
     }
